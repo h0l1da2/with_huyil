e2072f06c868221b34e66b1b2c0b40eba07910d6
h0l1da2 <whaleee@naver.com>
2023-01-26 17:47:19 +0900

    User 로그인 JWT 토큰 반환 완성 refresh 토큰 일단 세션에 저장으로 완성 (DB 에 받는 것으로 수정 계획) RolesVo 일부 수정 Mapper 일부 수정 Config 수정

--- src/main/java/com/with/hyuil/config/SecurityConfig.java
+++ src/main/java/com/with/hyuil/config/SecurityConfig.java
@@ -1,21 +1,40 @@
 package com.with.hyuil.config;
 
+import com.with.hyuil.config.auth.CustomUserDetailsService;
+import com.with.hyuil.config.jwt.JwtRequestFilter;
+import com.with.hyuil.config.jwt.JwtTokenProvider;
+import com.with.hyuil.dao.UsersMapper;
+import com.with.hyuil.service.UsersServiceImpl;
+import com.with.hyuil.service.interfaces.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UsersMapper usersMapper;
+    @Bean
+    public UsersService usersService() {
+        return new UsersServiceImpl(usersMapper, bCryptPasswordEncoder());
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(usersService());
+    }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -25,6 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(bCryptPasswordEncoder())
+                ;
     }
 
     @Override
@@ -38,10 +64,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
 
                 .authorizeRequests()
-                .mvcMatchers("/hosts/**")
-                .hasAnyRole("HOST", "ADMIN")
-                .mvcMatchers("/admin/**")
-                .hasRole("ADMIN")
+//                .mvcMatchers("/hosts/**")
+//                .hasAnyRole("HOST", "ADMIN")
+//                .mvcMatchers("/admin/**")
+//                .hasRole("ADMIN")
                 .anyRequest()
                 .permitAll()
 
@@ -53,8 +79,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/host/loginForm")
-//                .successHandler(hostAuthenticationProvider)
-//                .failureHandler()
                 .defaultSuccessUrl("/host")
 
                 .and()
@@ -64,13 +88,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
 
                 .and()
+                .addFilterBefore(new JwtRequestFilter(jwtTokenProvider, userDetailsService()), UsernamePasswordAuthenticationFilter.class)
         ;
+
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring() // 정적파일들 필터 검사 ㄴㄴ
-                .antMatchers("/resources/static/**")
+                .mvcMatchers("/static/**")
                 ;
     }
 }
