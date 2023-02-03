83cded4fe0478e3ed97f6cc6c939661e81aa04af
Hyuil Kang <116418443+h0l1da2@users.noreply.github.com>
2023-02-03 23:01:27 +0900

    파일 추가 (#27)
    * JWT 토큰 로그인 삭제
    시큐리티 세션 로그인으로 변경
    핸들러 외 추가
    
    * 호텔 insert 에러
    
    * hotel, hotel_info 등록
    
    * hotel mapper수정(db순서 추가)
    
    * hotelMapper (db순서 추가)
    
    * index 수정
    
    * @request /host 로 변경
    vo 부분 Long -> long 타입 변경
    room 등록완료admin_check_id 부분 db삭제해야돌아감(삭제안하면 admin fk를 못받아와서 에러발생)
    
    * commit
    
    * RoomMapper // Admin_check_id 널 들어가게 수정
    HotelMapper // SIDO 추가 (db에도 추가해줘야함)
    이전 푸쉬 수정사항 몇개반영안되어있어서 다시 푸쉬해요~
    
    * 시큐리티 로그인 / 호텔 검색 / 리스트 기능 추가 (#19)
    
    * utf-8 로 컨텐트타입 변경
    
    * add : 호텔 등록 관련, 시큐리티 로그인 관련 (#16)
    
    * JWT 토큰 로그인 삭제
    시큐리티 세션 로그인으로 변경
    핸들러 외 추가
    
    * 호텔 insert 에러
    
    * hotel, hotel_info 등록
    
    * index 수정
    
    ---------
    
    Co-authored-by: kim <kim@DESKTOP-P88B1P3>
    
    * 호텔 검색 추가
    (form 에서 input date 지만 컨트롤러에서 날짜 String 으로 받습니다 참고)
    페이지 핸들러 추가
    - 한 페이지 당 6개 데이터 보여줌
    - 페이지 목록은 5개씩
    - 다음/이전 목록 없을 경우 뒤로/앞으로 가기 버튼 안 뜹니다
    호텔 리스트와 서치용 Dto 추가
    호텔 매퍼 검색용 셀렉트 추가
    유저/호스트 로그인/조인 폼 등에 위드 휴일 누르면 각 인덱스 페이지 가게 수정
    
    ---------
    
    Co-authored-by: kim <kim@DESKTOP-P88B1P3>
    
    * hotelForm => // 이미지파일 db등록 가능
    
    * findByHotelId 수정
    
    ---------
    
    Co-authored-by: kim <kim@DESKTOP-P88B1P3>
    Co-authored-by: USER <USER@kosta02>
    Co-authored-by: NamSu <onsnara@naver.com>

--- src/main/java/com/with/hyuil/service/HotelServiceImpl.java
+++ src/main/java/com/with/hyuil/service/HotelServiceImpl.java
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4311133 (깃 충돌 수정중 (#24))
-
-
 package com.with.hyuil.service;
 
 import com.with.hyuil.dto.hotel.HotelListDto;
@@ -50,47 +41,3 @@ public class HotelServiceImpl implements HotelService {
 	}
 
 }
-<<<<<<< HEAD
-=======
->>>>>>> 78df10a (호텔 폼 / 호텔 검색 / 리스트 (#20))
-=======
-}
->>>>>>> 336955b (호텔폼 / 회원인포 기능 추가 (#23))
-=======
-
-
-package com.with.hyuil.service;
-
-import com.with.hyuil.dto.hotel.HotelListDto;
-import com.with.hyuil.dto.hotel.HotelSearchDto;
-import com.with.hyuil.service.interfaces.HotelService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.with.hyuil.dao.HotelMapper;
-import com.with.hyuil.model.HotelVo;
-import java.util.List;
-
-@Slf4j
-@Service
-public class HotelServiceImpl implements HotelService {
-
-	@Autowired
-	private HotelMapper mapper;
-
-	@Override
-	public void addHotel(HotelVo hotelvo) {
-		mapper.insertHotel(hotelvo);
-	}
-	@Override
-	public List<HotelListDto> searchHotels(HotelSearchDto hotelSearchDto) {
-		log.info("CheckIn = {}", hotelSearchDto.getCheckIn());
-		log.info("CheckIn = {}", hotelSearchDto.getCheckOut());
-		return mapper.selectForHotelList(hotelSearchDto);
-	}
-
-}
-
->>>>>>> b1c1dc2 (깃 충돌 수정중 (#24))
->>>>>>> 4311133 (깃 충돌 수정중 (#24))
--- src/main/java/com/with/hyuil/service/interfaces/HotelService.java
+++ src/main/java/com/with/hyuil/service/interfaces/HotelService.java
@@ -10,21 +10,7 @@ public interface HotelService {
 
     void addHotel(HotelVo hotelvo);
     List<HotelListDto> searchHotels(HotelSearchDto hotelSearchDto);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4311133 (깃 충돌 수정중 (#24))
-	HotelVo findByHotelId(long id);
-    Integer searchHotelsCnt(HotelSearchDto hotelSearchDto);
-<<<<<<< HEAD
-=======
->>>>>>> 78df10a (호텔 폼 / 호텔 검색 / 리스트 (#20))
-=======
->>>>>>> 336955b (호텔폼 / 회원인포 기능 추가 (#23))
-=======
+    HotelVo findByHotelUserId(Long userId);
+    HotelVo findByHotelId(long id);
 
->>>>>>> b1c1dc2 (깃 충돌 수정중 (#24))
->>>>>>> 4311133 (깃 충돌 수정중 (#24))
 }
